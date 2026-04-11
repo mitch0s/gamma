04ab41ddda2fae804ed0b1ffd89b250d27f0772b
Mitch Naake <mitchnaake@gmail.com>
2026-04-11 10:19:36 +0800

    encapsulate terminal/server code into Gamma class for simple invokation

--- src/gamma/gamma.py
+++ src/gamma/gamma.py
@@ -1,38 +1,51 @@
-# import socket
-# import time
-# import src.gamma as gamma
+import logging
+import asyncio
+from gamma.netty.player_connection import PlayerConnection
+from gamma.netty.server_connection import ServerConnection
+from gamma.netty.connection_relay import ConnectionRelay
+from gamma.gui.terminal import GammaTerminal
+from gamma.mixin.logger import CallbackHandler
+# logging.basicConfig = lambda *a, **k: None  # patch first
+root = logging.getLogger()
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[CallbackHandler()]  # add logging.StreamHandler() for console output
+)
 
+class Gamma:
+    def __init__(self, host:str='127.0.0.1', port:int=25565):
+        self.host = host
+        self.port = port
 
-# class Gamma:
-#     debug:bool = False
-#     config:dict = {}
-#     socket: socket.socket
-#     event_func:list = []
+    def start(self):
+        asyncio.run(self.start_async())
 
-#     def bind(self, host:str='localhost', port:int=25565):
-#         """
-#          Create the TCP Socket Server.
-#         """
-#         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         if self.debug : print('[DEBUG] Created socket server')
-#         if self.debug: print(f'[DEBUG] Attempting to bind to port ({host}:{port})')
-#         while True:
-#             try:
-#                 self.socket.bind((host, port))
-#                 break
+    async def start_async(self):
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(self.terminal_task())
+            tg.create_task(self.server_task())
+    
+    async def terminal_task(self):
+        interface = GammaTerminal()
+        await interface.run_async()
+    
+    async def server_task(self):
+        server = await asyncio.start_server(self.handle_player, self.host, self.port)
+        async with server:
+            await server.serve_forever()
+        
 
-#             except:
-#                 time.sleep(0.2)
-#         if self.debug: print(f'[DEBUG] Successfully bound to port ({host}:{port})')
+    async def handle_player(self, reader, writer):
+        async def _handle(reader, writer):
+            try:
+                player_conn = PlayerConnection(reader, writer)
+                server_conn = ServerConnection(host='localhost', port=25560)
+                relay = ConnectionRelay(downstream=player_conn, upstream=server_conn)
+                await relay.start()
+            except (ConnectionResetError, BrokenPipeError, EOFError, asyncio.CancelledError):
+                pass
+            finally:
+                if not writer.is_closing():
+                    writer.close()
+        asyncio.create_task(_handle(reader, writer))
 
-
-#     def listen(self):
-#         """
-#          Start accepting player connections.
-#         """
-#         self.socket.listen()
-#         if self.debug: print(f'[DEBUG] Socket server listening')
-#         while True:
-#             downstream_conn, downstream_addr = self.socket.accept()
-#             if downstream_conn:
-#                 gamma.Connection(downstream_conn=downstream_conn, downstream_addr=downstream_addr)
--- src/main.py
+++ src/main.py
@@ -1,48 +1,10 @@
-import logging
-from gamma.mixin.logger import CallbackHandler
-# logging.basicConfig = lambda *a, **k: None  # patch first
-root = logging.getLogger()
-logging.basicConfig(
-    level=logging.INFO,
-    handlers=[CallbackHandler()]  # add logging.StreamHandler() for console output
-)
+from gamma import Gamma
 
-import asyncio
-from gamma.netty.player_connection import PlayerConnection
-from gamma.netty.server_connection import ServerConnection
-from gamma.netty.connection_relay import ConnectionRelay
-from gamma.gui.terminal import GammaTerminal
 
-async def server_task(): 
-    async def handle_player(reader, writer):
-        asyncio.create_task(_handle(reader, writer))
-
-    async def _handle(reader, writer):
-        try:
-            player_conn = PlayerConnection(reader, writer)
-            server_conn = ServerConnection(host='localhost', port=25560)
-            relay = ConnectionRelay(downstream=player_conn, upstream=server_conn)
-            await relay.start()
-        except (ConnectionResetError, BrokenPipeError, EOFError, asyncio.CancelledError):
-            pass
-        finally:
-            if not writer.is_closing():
-                writer.close()
-
-    server = await asyncio.start_server(handle_player, '0.0.0.0', 25565)
-    async with server:
-        await server.serve_forever()
-
-async def terminal_task():
-    interface = GammaTerminal()
-    await interface.run_async()
-
-async def main():
-    async with asyncio.TaskGroup() as tg:
-        tg.create_task(terminal_task())
-        tg.create_task(server_task())
-        root.error('TEST')
+def main():
+    proxy = Gamma(host='0.0.0.0', port=25565)
+    proxy.start()
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
